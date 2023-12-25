e71c26aa4f6a8d57a2f14df01fa71529d8352e8d
robert <4r.iskey@gmail.com>
2023-12-25 19:19:52 +0400

    style: - added transition on routes change

--- apps/host/src/app/App.tsx
+++ apps/host/src/app/App.tsx
@@ -3,23 +3,36 @@ import Navigation from "./components/navigation/Navigation";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
 import { appRoutes } from "./appRoutes";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
+import { GlobalStyles } from "./App.styled";
 
 export function App() {
+  const location = useLocation();
+
   return (
-    <React.Suspense fallback={null}>
+    <div style={{position: 'relative'}}>
+      <GlobalStyles />
       <Navigation />
-      <Main>
-        <Routes>
-          {
-            appRoutes.map(route =>
-              <Route key={route.path} index={route.index} path={route.path} element={route.element} />
-            )
-          }
-        </Routes>
-      </Main>
-      <Footer />
-    </React.Suspense>
+      <SwitchTransition>
+        <CSSTransition key={location.key} timeout={300} unmountOnExit classNames="page">
+          <React.Suspense fallback={null}>
+            <Main>
+              <Routes location={location}>
+                {
+                  appRoutes.map(route =>
+                    <Route key={route.path} index={route.index} path={route.path} element={route.element} />
+                  )
+                }
+                <Route path="*" element={null} />
+              </Routes>
+            </Main>
+            <Footer />
+          </React.Suspense>
+        </CSSTransition>
+      </SwitchTransition>
+    </div>
+
   );
 }
 
